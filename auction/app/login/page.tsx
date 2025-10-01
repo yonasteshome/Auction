c5b0ea93e1e7216db79d1d8770204828e3ac22ff
Yonas Teshome <140391850+yonasteshome@users.noreply.github.com>
2025-10-01 11:56:54 +0300

    Update page.tsx

--- auction/app/login/page.tsx
+++ auction/app/login/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault()
     try {
-      const res = await fetch("http://localhost:5000/api/users/login", {
+      const res = await fetch("https://auction-xi-five.vercel.app/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
