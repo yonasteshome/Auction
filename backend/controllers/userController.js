6a0a956fd6eed6b391b3ba5f20210fcbb87fe5ad
Yonas Teshome <140391850+yonasteshome@users.noreply.github.com>
2025-10-01 14:35:21 +0300

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -71,7 +71,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // only over HTTPS in prod
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
